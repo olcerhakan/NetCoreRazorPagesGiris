2889ff9f297152fb01168465fd223f30ade3e938
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-10 18:39:15 +0300

    Add project files.

---
+++ RazorPagesDeneme/Models/OkulContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesDeneme.Models
+{
+    public class OkulContext : DbContext
+    {
+        public OkulContext(DbContextOptions<OkulContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Kisi> Kisiler { get; set; }
+    }
+}
---
+++ RazorPagesDeneme/Pages/Hakan.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesDeneme.Pages
+{
+    public class HakanModel : PageModel
+    {
+        public string Sonuc { get; set; }
+        public string GozRengi { get; set; }
+        public void OnGet(bool? dogruMu)
+        {
+            ViewData["ad"] = "Hakan";
+
+            GozRengi = "Siyah";
+
+            if (dogruMu.HasValue)
+            {
+                Sonuc = dogruMu.Value ? "Tebrikler bildiniz." : "Malesef, bilemediniz.";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            bool result = Quiz.Cevap.ToLower() == "ölçer";
+            return RedirectToPage("./Hakan", new { dogruMu = result });
+        }
+
+        [BindProperty]
+        public Quiz Quiz { get; set; }
+    }
+
+    public class Quiz
+    {
+        [Required]
+        public string Cevap { get; set; }
+
+
+    }
+}
